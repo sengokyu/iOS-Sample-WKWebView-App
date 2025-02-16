bb75515bd8a9c4cdff7778e4364183a8f9ab69ca
sengokyu <sengokyu+gh@gmail.com>
2025-02-16 23:56:22 +0900

    Update ContentView.swift

--- ExWebKit/ContentView.swift
+++ ExWebKit/ContentView.swift
@@ -9,18 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        WebView(url: URL(string: "https://www.apple.com/")!)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
