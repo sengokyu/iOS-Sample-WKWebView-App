61c36d7ab7cc417cb11e6c0d217f971647080559
sengokyu <sengokyu+gh@gmail.com>
2025-02-16 19:58:23 +0900

    Create project

---
+++ ExWebKit/ExWebKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExWebKitApp.swift
+//  ExWebKit
+//
+//  Created by ziyi on 2025/02/16.
+//
+
+import SwiftUI
+
+@main
+struct ExWebKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
